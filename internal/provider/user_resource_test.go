b9b93831a3b90248d44b1e1fa5ace7977e63e9e1
Oliver Binns <mail@oliverbinns.co.uk>
2025-04-19 15:19:57 +0100

    test: use random email for tests to allow parallel running

--- internal/provider/user_resource_test.go
+++ internal/provider/user_resource_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
@@ -11,6 +12,11 @@ import (
 )
 
 func TestAccUserResource(t *testing.T) {
+	accountEmail := fmt.Sprintf(
+		"%s@oliverbinns.co.uk",
+		uuid.New().String(),
+	)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -18,18 +24,24 @@ func TestAccUserResource(t *testing.T) {
 			// Create and Read testing
 			{
 				Config: testAccUserResourceConfig(
+					accountEmail,
 					`"CAN_SEE_ALL_APPS", "CAN_REPLY_TO_REVIEWS_GLOBAL"`,
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
 						tfjsonpath.New("email"),
-						knownvalue.StringExact("example@oliverbinns.co.uk"),
+						knownvalue.StringExact(accountEmail),
 					),
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
 						tfjsonpath.New("name"),
-						knownvalue.StringExact("developers/5166846112789481453/users/example@oliverbinns.co.uk"),
+						knownvalue.StringExact(
+							fmt.Sprintf(
+								"developers/5166846112789481453/users/%s",
+								accountEmail,
+							),
+						),
 					),
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
@@ -44,18 +56,24 @@ func TestAccUserResource(t *testing.T) {
 			// Update and Read testing
 			{
 				Config: testAccUserResourceConfig(
+					accountEmail,
 					`"CAN_VIEW_NON_FINANCIAL_DATA_GLOBAL"`,
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
 						tfjsonpath.New("email"),
-						knownvalue.StringExact("example@oliverbinns.co.uk"),
+						knownvalue.StringExact(accountEmail),
 					),
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
 						tfjsonpath.New("name"),
-						knownvalue.StringExact("developers/5166846112789481453/users/example@oliverbinns.co.uk"),
+						knownvalue.StringExact(
+							fmt.Sprintf(
+								"developers/5166846112789481453/users/%s",
+								accountEmail,
+							),
+						),
 					),
 					statecheck.ExpectKnownValue(
 						"googleplay_user.oliver",
@@ -71,10 +89,10 @@ func TestAccUserResource(t *testing.T) {
 	})
 }
 
-func testAccUserResourceConfig(permissions string) string {
+func testAccUserResourceConfig(accountEmail string, permissions string) string {
 	return fmt.Sprintf(`
 resource "googleplay_user" "oliver" {
-  email = "example@oliverbinns.co.uk"
+  email = "%s"
   permissions = [
     %s
   ]
@@ -83,5 +101,5 @@ resource "googleplay_user" "oliver" {
 provider "googleplay" {
   service_account_json_base64 = filebase64("~/service-account.json")
   developer_id = "5166846112789481453"
-}`, permissions)
+}`, accountEmail, permissions)
 }
