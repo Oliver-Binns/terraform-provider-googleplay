a05d1c7c4f1d57cdf6e462922832a73f085d0f51
Oliver Binns <mail@oliverbinns.co.uk>
2026-06-04 10:54:45 +0100

    fix: Add CAN_EDIT_CONNECTED_APPS_GLOBAL to user permissions

--- internal/provider/user_resource_test.go
+++ internal/provider/user_resource_test.go
@@ -144,6 +144,7 @@ func TestAccUserResource(t *testing.T) {
 							knownvalue.StringExact("CAN_VIEW_APP_QUALITY_GLOBAL"),
 							knownvalue.StringExact("CAN_MANAGE_DEEPLINKS_GLOBAL"),
 							knownvalue.StringExact("CAN_VIEW_CONNECTED_APPS_GLOBAL"),
+							knownvalue.StringExact("CAN_EDIT_CONNECTED_APPS_GLOBAL"),
 						}),
 					),
 				},
